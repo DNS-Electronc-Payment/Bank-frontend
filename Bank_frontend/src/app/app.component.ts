aae07ba03b00a17d069490278ee23bd393a7257a
Ninoli1 <nikolinaskiljevic@gmail.com>
2024-11-25 23:10:39 +0100

    feat: web socket

--- Bank_frontend/src/app/app.component.ts
+++ Bank_frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { CardDataEnteringComponent } from './card-data-entering/card-data-entering/card-data-entering.component';
 
 
@@ -19,4 +19,60 @@ import { CardDataEnteringComponent } from './card-data-entering/card-data-enteri
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  title='Bank_frontend'
+
+  private webSocket!: WebSocket;
+  private webSocketClient!: WebSocket;
+  private webSocketResponse!: WebSocket;
+
+  constructor(private router:Router){
+    this.initializeWebSockets();
+  }
+
+  initializeWebSockets(){
+    this.setupWebSocket("responses")
+  }
+
+  private setupWebSocket(endpoint: string) {
+    const url = `ws://localhost:8082/${endpoint}`;
+    const webSocket = new WebSocket(url);
+
+    webSocket.onopen = () => {
+      console.log(`WebSocket connection to ${endpoint} established.`);
+    };
+
+    webSocket.onclose = (event) => {
+      console.log(`WebSocket connection to ${endpoint} closed. Reconnecting...`);
+      setTimeout(() => this.setupWebSocket(endpoint), 1000); // Ponovni pokušaj nakon 5 sekundi
+    };
+
+    webSocket.onerror = (error) => {
+      console.error(`WebSocket error on ${endpoint}:`, error);
+      webSocket.close(); // Zatvori konekciju da bi se pokrenuo onclose handler
+    };
+
+    webSocket.onmessage = (event) => {
+      console.log(`Message from ${endpoint}:`, event.data);
+      // Obradi poruku na osnovu endpoint-a
+      if (endpoint === 'responses') {
+        this.handleTransactionMessage(event);
+      }
+    };
+
+    // Sačuvaj referencu na WebSocket
+    if (endpoint === 'transactions') {
+      this.webSocket = webSocket;
+    } else if (endpoint === 'clients') {
+      this.webSocketClient = webSocket;
+    } else if (endpoint === 'responses') {
+      this.webSocketResponse = webSocket;
+    }
+  }
+
+  private handleTransactionMessage(event: MessageEvent) {
+    console.log(event.data);
+    this.router.navigate(['/form']);
+   
+  }
+}
--- Bank_frontend/src/app/app.routes.ts
+++ Bank_frontend/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { CardDataEnteringComponent } from './card-data-entering/card-data-entering/card-data-entering.component';
 
 export const routes: Routes = [
-    { path: '', component: CardDataEnteringComponent }, 
+    { path: 'form', component: CardDataEnteringComponent }, 
 ];
